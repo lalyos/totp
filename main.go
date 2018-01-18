dd8e0acebe1c923fb52ae32394a5e96aef6096ad
lalyos <lalyos@yahoo.com>
2018-01-18 15:03:26 +0100

    add version and clipboard flags

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	otp "github.com/hgfischer/go-otp"
+	"github.com/tjgq/clipboard"
 )
 
 //var version, gitrev string
@@ -20,11 +21,25 @@ func init() {
 }
 
 const TOKEN_ENV_NAME = "TOTP_TOKEN"
+const usage = `
+usage: totp <token-secret>
+   either use the first argument to set the totp secret,
+   or use the TOTP_TOKEN env variable
+`
+
+var clpb bool
 
 func main() {
-	fmt.Println(totp.Get())
-	fmt.Println("version", version)
+	vers := flag.Bool("version", false, "prints version")
+	flag.BoolVar(&clpb, "clipboard", false, "copies token to system clipboard")
+	flag.BoolVar(&clpb, "c", false, "(short) copies token to system clipboard")
 	flag.Parse()
+
+	if *vers {
+		fmt.Printf("version: %v-%v\n", version, gitrev)
+		os.Exit(0)
+	}
+
 	token := os.Getenv(TOKEN_ENV_NAME)
 	if token == "" {
 		token = flag.Arg(0)
@@ -35,4 +50,10 @@ func main() {
 	}
 
 	totp := &otp.TOTP{Secret: strings.ToUpper(token), IsBase32Secret: true}
+	if clpb {
+		clipboard.Set(totp.Get())
+		fmt.Fprintln(os.Stderr, "totp token writen to CLIPBOARD")
+		os.Exit(0)
+	}
+	fmt.Print(totp.Get())
 }
